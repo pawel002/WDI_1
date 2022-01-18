b7c023426a9abb8c216d607c08d4341d030056d0
pawel002 <pawel.gandalf@gmail.com>
2022-01-18 17:00:42 +0100

    push

--- 10.py
+++ 10.py
@@ -61,9 +61,54 @@ class Number:
         return output
 
 
+def reverse(head: Node):
+    prev = None
+    current = head
+    while current is not None:
+        new = current.next
+        current.next = prev
+        prev = current
+        current = new
+    return prev
+
+
+def node_printer(head: Node):
+    while head is not None:
+        if head.next is not None:
+            print(str(head) + ", ", end="")
+        else:
+            print(head)
+        head = head.next
+
+
+# Ans to 10
+def add(head1: Node, head2: Node):
+    tail1 = reverse(head1)
+    tail2 = reverse(head2)
+    tail = Node()
+    Tail = tail
+    remainder = 0
+    while tail1 and tail2:
+        tail.next = Node((tail1.val + tail2.val + remainder) % 10)
+        remainder = (tail1.val + tail2.val + remainder) // 10
+        tail, tail1, tail2 = tail.next, tail1.next, tail2.next
+    while tail1:
+        tail.next = Node((tail1.val + remainder) % 10)
+        remainder = (tail1.val + remainder) // 10
+        tail, tail1 = tail.next, tail1.next
+    while tail2:
+        tail.next = Node((tail2 + remainder) % 10)
+        remainder = (tail2.val + remainder) // 10
+        tail, tail2 = tail.next, tail2.next
+    if remainder != 0:
+        tail.next = Node(1)
+    return reverse(Tail.next)
+
+
 
 n = Number()
-n.set(9999)
-print(n)
-n.add1()
-print(n)
+n.set(99)
+m = Number()
+m.set(1)
+node_printer(add(n.first, m.first))
+
